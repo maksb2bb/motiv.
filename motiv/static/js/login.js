5e0fd77cf0b7717c47e4d497e6b598b9d00a3e38
maksb2bb <114099845+maksb2bb@users.noreply.github.com>
2023-01-25 03:29:55 +0300

    Add files via upload

---
+++ motiv/static/js/login.js
@@ -0,0 +1,26 @@
+const email = document.querySelector("#email")
+const pass = document.querySelector("#pass")
+const btn = document.querySelector("#btn")
+
+if (email.value == "gein@sfedu.ru"){
+    console.log('OK');
+}
+else{
+    console.log("You pidor")
+    
+}
+
+if (pass.value == "qqq"){
+    console.log('OK');
+}
+else{
+    console.log("You chlen")
+}
+
+
+
+
+
+
+
+
